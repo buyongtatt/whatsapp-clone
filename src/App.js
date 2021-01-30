5d1e4a3589d5ed8a99e26a91b2cd2d3a4c0e6ebf
021120 <yongtatt981213@hotmail.com>
2021-01-30 17:40:06 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+
+      <div className="app__body">
+        {/* Sidebar */}
+        {/* Chat */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDjUt6ubTenMdeBAb0hpf5sC3Eu9J5hsxc",
+    authDomain: "whatsapp-clone-974a6.firebaseapp.com",
+    projectId: "whatsapp-clone-974a6",
+    storageBucket: "whatsapp-clone-974a6.appspot.com",
+    messagingSenderId: "202210400339",
+    appId: "1:202210400339:web:547ccbcf426a4ad76b4ea6",
+    measurementId: "G-VB469WEQQ8"
+  };
